3ecc9b14a9527835b99c27890b6585fb40c134e9
Ilya Grigorik <ilya@igvita.com>
2014-01-05 11:35:12 -0800

    log GA hit in the background (via GAE task)
    - https://developers.google.com/appengine/docs/go/taskqueue/delay
    - https://developers.google.com/appengine/docs/quotas#Task_Queue

--- ga-beacon/ga-beacon.go
+++ ga-beacon/ga-beacon.go
@@ -47,6 +47,32 @@ func generateUUID() string {
 	return hex.EncodeToString(b)
 }
 
+var delayHit = delay.Func("collect", logHit)
+
+func logHit(c appengine.Context, params []string, ua string, cid string) error {
+	// https://developers.google.com/analytics/devguides/collection/protocol/v1/reference
+	payload := url.Values{
+		"v":   {"1"},        // protocol version = 1
+		"t":   {"pageview"}, // hit type
+		"tid": {params[0]},  // tracking / property ID
+		"cid": {cid},        // unique client ID (server generated UUID)
+		"dp":  {params[1]},  // page path
+	}
+
+	req, _ := http.NewRequest("POST", beaconURL, strings.NewReader(payload.Encode()))
+	req.Header.Add("User-Agent", ua)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	if resp, err := urlfetch.Client(c).Do(req); err != nil {
+		c.Errorf("GA collector POST error: %s", err.Error())
+		return err
+	} else {
+		c.Debugf("GA collector status: %v, cid: %v", resp.Status, cid)
+	}
+
+	return nil
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
 	params := strings.SplitN(strings.Trim(r.URL.Path, "/"), "/", 2)
@@ -84,8 +110,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "image/gif")
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("CID", cid)
-	
-	collectFunc.call(c, r, params, cid)
+
+	// logHit(c, params, r.Header.Get("User-Agent"), cid)
+	delayHit.Call(c, params, r.Header.Get("User-Agent"), cid)
 
 	// Write out GIF pixel or badge, based on presence of "pixel" param.
 	query, _ := url.ParseQuery(r.URL.RawQuery)
@@ -96,25 +123,3 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-var collectFunc = delay.Func("collectHit", func(c appengine.Context, r *http.Request, params []string, cid string) error {
-	// https://developers.google.com/analytics/devguides/collection/protocol/v1/reference
-	payload := url.Values{
-		"v":   {"1"},        // protocol version = 1
-		"t":   {"pageview"}, // hit type
-		"tid": {params[0]},  // tracking / property ID
-		"cid": {cid},        // unique client ID (server generated UUID)
-		"dp":  {params[1]},  // page path
-	}
-
-	req, _ := http.NewRequest("POST", beaconURL, strings.NewReader(payload.Encode()))
-	req.Header.Add("User-Agent", r.Header.Get("User-Agent"))
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
-	if resp, err := urlfetch.Client(c).Do(req); err != nil {
-		c.Errorf("GA collector POST error: %s", err.Error())
-		return err
-	}
-	
-	c.Debugf("GA collector status: %v, cid: %v", resp.Status, cid)
-	return nil
-})

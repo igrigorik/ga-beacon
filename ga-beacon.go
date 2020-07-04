0e67f958304bc89345431fa9ffcb19a48507f772
Alok Choudhary <alok.c83@gmail.com>
2020-07-04 17:10:04 -0500

    required main fucntion

--- ga-beacon.go
+++ ga-beacon.go
@@ -1,4 +1,4 @@
-package beacon
+package main
 
 import (
 	"crypto/rand"
@@ -29,6 +29,20 @@ var (
 
 func init() {
 	http.HandleFunc("/", handler)
+}
+
+//required 
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func mustReadFile(path string) []byte {
